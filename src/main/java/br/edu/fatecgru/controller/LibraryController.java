d2d179576a10a07ce84027f98878cbbeedeaebb5
Sara Robles <sararoblesbarbosa2@gmail.com>
2025-10-11 15:42:10 -0300

    update pom file

--- src/main/java/br/edu/fatecgru/controller/LibraryController.java
+++ src/main/java/br/edu/fatecgru/controller/LibraryController.java
@@ -1,4 +1,14 @@
 package br.edu.fatecgru.controller;
 
+import javafx.event.ActionEvent;
+
 public class LibraryController {
+
+    public void cadastrarLivro(ActionEvent event) {
+        System.out.println("Cadastrando livro...");
+    }
+
+    public void listarLivros(ActionEvent event) {
+        System.out.println("Listando livros...");
+    }
 }
--- src/main/java/br/edu/fatecgru/ui/LibraryInterface.java
+++ src/main/java/br/edu/fatecgru/ui/LibraryInterface.java
@@ -1,15 +1,22 @@
 package br.edu.fatecgru.ui;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class LibraryInterface extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/ui/home.fxml"));
+        Scene scene = new Scene(root, 400, 300);
+        stage.setTitle("Biblioteca FATEC");
+        stage.setScene(scene);
+        stage.show();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        
+    public static void main(String[] args) {
+        launch(args);
     }
 }
