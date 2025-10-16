92d9689d7a89d3f1ba530487f1b644779f02f3a5
Sara Robles <sararoblesbarbosa2@gmail.com>
2025-10-15 22:19:53 -0300

    create menu bar structure

--- src/main/java/br/edu/fatecgru/ui/LibraryInterface.java
+++ src/main/java/br/edu/fatecgru/ui/LibraryInterface.java
@@ -9,12 +9,22 @@ public class LibraryInterface extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/ui/home.fxml"));
-        Scene scene = new Scene(root, 400, 300);
-        stage.setTitle("Biblioteca FATEC");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ui/home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
+
+        scene.getStylesheets().add(getClass().getResource("/css/styles.css").toExternalForm());
+
+        stage.setTitle("Bibliotec");
+
         stage.setScene(scene);
+
+        stage.setMaximized(true);
+
+        stage.setResizable(true);
+
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
