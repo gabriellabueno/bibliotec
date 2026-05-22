6a6a0737da84afb546795132d612739698fbef01
Sara Robles <sararoblesbarbosa2@gmail.com>
2026-05-22 17:40:09 -0300

    Insere dicas na home

--- src/main/java/br/edu/fatecgru/controller/HomeController.java
+++ src/main/java/br/edu/fatecgru/controller/HomeController.java
@@ -10,6 +10,14 @@ import java.io.IOException;
 public class HomeController {
 
     @FXML
+    public void initialize() {
+        if(mainBorderPane != null) {
+            loadScreen("/ui/screens/home.fxml");
+        }
+
+    }
+
+    @FXML
     private BorderPane mainBorderPane;
 
     private void loadScreen(String fxmlFile) {
@@ -24,56 +32,5 @@ public class HomeController {
     }
 
 
-
-    @FXML
-    private void onHomeClick() {
-        loadScreen("/ui/screens/home.fxml");
-    }
-
-    @FXML
-    private void onSearchMaterialClick() {
-        loadScreen("/ui/screens/pesquisa/pesquisa-material.fxml");
-    }
-
-    @FXML
-    private void onSearchUserClick() {
-        loadScreen("/ui/screens/pesquisa/pesquisa-usuario.fxml");
-    }
-
-    @FXML
-    private void onRegisterMaterialClick() {
-
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("resources/ui/screens/cadastro-material.fxml"));
-
-            Pane newScreen = loader.load();
-            mainBorderPane.setCenter(newScreen);
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("Erro ao carregar tela: " + "/ui/screens/cadastro/cadastro-material.fxml");
-        }
-
-    }
-
-    @FXML
-    private void onRegisterNoteClick() {
-        loadScreen("/ui/screens/cadastro/cadastro-note.fxml");
-    }
-
-    @FXML
-    private void onRegisterLoanClick() {
-        loadScreen("/ui/screens/cadastro/cadastro-emprestimo.fxml");
-    }
-
-    @FXML
-    private void onRegisterUserClick() {
-        loadScreen("/ui/screens/cadastro/cadastro-usuario.fxml");
-    }
-
-    @FXML
-    public void initialize() {
-
-        loadScreen("/ui/screens/home.fxml");
-    }
 
 }
